ddde7a306833f33a164b769a12fab396869b14c8
Marcele  Montalvão <marcelemontalvao@MacBook-Pro-de-Marcele-2.local>
2025-03-06 19:34:07 -0300

    Adicionando configurações do banco no application.properties

---
+++ src/main/java/com/vrsoftware/gerenciadorDeVendas/GerenciadorDeVendasApplication.java
@@ -0,0 +1,13 @@
+package com.vrsoftware.gerenciadorDeVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeVendasApplication.class, args);
+	}
+
+}
